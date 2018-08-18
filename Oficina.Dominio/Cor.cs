4f2f9fc77fe2a84bd32a4c3f5992e940b992aade
impacta.treinamentos <impacta.treinamentos@hotmail.com>
2018-08-17 22:28:06 -0300

    Initial commit

---
+++ Oficina.Dominio/Cor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Oficina.Dominio
+{
+    public class Cor
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public void Add(Cor cor)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Oficina.Repositorios.SistemaArquivosTests/VeiculorepositorioTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Oficina.Dominio;
+using Oficina.Repositorios.SistemaArquivos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oficina.Repositorios.SistemaArquivos.Tests
+{
+    [TestClass()]
+    public class VeiculorepositorioTests
+    {
+        [TestMethod()]
+        public void InserirTest()
+        {
+            var veiculo = new Veiculo();
+            
+            veiculo.Ano = 2018;
+            veiculo.Cambio = Cambio.Manual;
+            veiculo.Combustivel = Combustivel.Flex;
+            veiculo.Cor = new CorRepositorio().Selecionar(2);
+            veiculo.Modelo = new ModeloRepositorio().Selecionar(3);
+            veiculo.Placa = "ENR-5975";
+            veiculo.Observacao = "Observação!!";
+
+            new VeiculoRepositorio().Inserir(veiculo);
+        }
+    }
+}
